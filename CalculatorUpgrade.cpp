8a30b28d86c39f0eec01a6f31007f4d2c6401896
rachapol-prachya <68219010001@udontech.ac.th>
2025-07-30 11:24:17 +0700

    Create CalculatorUpgrade.cpp

---
+++ CalculatorUpgrade.cpp
@@ -0,0 +1,78 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;//
+int main() {
+    // Write C++ code here
+    cout << "Calculator" << endl;
+    int op;
+    int num1,num2;
+    cin >> op;
+    if(op==1) {
+        cout << "Adder function" << endl;
+        cout << "Enter num1 : ";
+        cin >> num1;
+        cout << endl;
+        // num2
+        cout << "Enter num2 : ";
+        cin >> num2;
+        cout << endl;
+        cout << "----------------------------" << endl;
+        cout << num1 << " + " << num2 << " = " << num1+num2 << endl;
+    } else if (op==2) {
+        cout << "Subtract Function" << endl;
+
+        cout << "Enter num1 : ";
+        cin >> num1;
+        cout << endl;
+        // num2
+        cout << "Enter num2 : ";
+        cin >> num2;
+        cout << endl;
+        cout << "----------------------------" << endl;
+        cout << num1 << " - " << num2 << " = " << num1-num2 << endl;
+        
+    } else if (op==3) {
+        cout << "Muitply Function" << endl;
+
+        cout << "Enter num1 : ";
+        cin >> num1;
+        cout << endl;
+        // num2
+        cout << "Enter num2 : ";
+        cin >> num2;
+        cout << endl;
+        cout << "----------------------------" << endl;
+        cout << num1 << " * " << num2 << " = " << num1*num2 << endl;
+        
+    } else if (op==4) {
+        cout << "Divie Function" << endl;
+
+        cout << "Enter num1 : ";
+        cin >> num1;
+        cout << endl;
+        // num2
+        cout << "Enter num2 : ";
+        cin >> num2;
+        cout << endl;
+        cout << "----------------------------" << endl;
+        cout << num1 << " / " << num2 << " = " << num1/num2 << endl;
+        
+    } else if (op==5) {
+        cout << "modulation Function" << endl;
+
+        cout << "Enter num1 : ";
+        cin >> num1;
+        cout << endl;
+        // num2
+        cout << "Enter num2 : ";
+        cin >> num2;
+        cout << endl;
+        cout << "----------------------------" << endl;
+        cout << num1 << " % " << num2 << " = " << num1%num2 << endl;
+        
+    } else {
+        cout << "Invalid data!" << endl;
+    }
+    
+    return 0;
+}
